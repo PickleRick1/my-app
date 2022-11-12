c2e87b3e260f414222a74b4656ab78180578af98
Olga Bordunova <memofpass21@gmail.com>
2022-11-12 15:35:13 +0300

    splitting into components

---
+++ src/components/Profile.jsx
@@ -0,0 +1,14 @@
+const Profile = () => {
+	return (
+		<div className='content'>
+			<div><img src="https://avatars.mds.yandex.net/i?id=ac60c484f2b28bbad74fb98a161220753732fb0e-4360535-images-thumbs&n=13" alt="img" /></div>
+			<div> ava</div>
+			<div> myposts
+				<div>new post</div>
+				<div>post 1</div>
+				<div>post 2</div>
+			</div>
+		</div>
+	)
+}
+export default Profile;
