eefe47397ccfaf2438ee58312dca8e395370999b
Olga Bordunova <memofpass21@gmail.com>
2022-11-13 20:53:12 +0300

    create css-modules

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,15 @@
+import s from './Nav.module.css';
+
 const Nav = () => {
 	return (
-		<nav className='nav'>
-			<div><a href='#'>Profile</a></div>
-			<div><a href='#'>Messages</a></div>
-			<div><a href='#'>News</a></div>
-			<div><a href='#'>Music</a></div>
-			<div><a href='#'>Settings</a></div>
-
+		<nav className={s.nav}>
+			<ul className={s.menu}>
+				<li className={s.menu__item}><a href='#'>Profile</a></li>
+				<li className={s.menu__item}><a href='#'>Messages</a></li>
+				<li className={s.menu__item}><a href='#'>News</a></li>
+				<li className={s.menu__item}><a href='#'>Music</a></li>
+				<li className={s.menu__item}><a href='#'>Settings</a></li>
+			</ul>
 		</nav>
 	)
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,8 @@
+
+import s from './Profile.module.css';
 const Profile = () => {
 	return (
-		<div className='content'>
+		<div className={s.content}>
 			<div><img src="https://avatars.mds.yandex.net/i?id=ac60c484f2b28bbad74fb98a161220753732fb0e-4360535-images-thumbs&n=13" alt="img" /></div>
 			<div> ava</div>
 			<div> myposts
