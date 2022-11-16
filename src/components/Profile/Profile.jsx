92d1ae2aa7f33cf34403d44ba565c85561e210d0
Olga Bordunova <memofpass21@gmail.com>
2022-11-16 20:23:43 +0300

    creating page Dialogs

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import MyPosts from './MyPosts/MyPosts';
 import s from './Profile.module.css';
 const Profile = () => {
 	return (
-		<div className={s.content}>
+		<div>
 			<div><img src="https://avatars.mds.yandex.net/i?id=ac60c484f2b28bbad74fb98a161220753732fb0e-4360535-images-thumbs&n=13" alt="img" /></div>
 			<div> ava</div>
 			<MyPosts />
