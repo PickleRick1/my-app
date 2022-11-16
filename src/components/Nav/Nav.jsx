7e5c482db439369220f57279597242dd713af4ba
Olga Bordunova <memofpass21@gmail.com>
2022-11-16 21:50:53 +0300

    NavLink and activeClass

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,16 +1,17 @@
+import { NavLink } from 'react-router-dom';
 import s from './Nav.module.css';
 
 const Nav = () => {
 	return (
 		<nav className={s.nav}>
 			<ul className={s.menu}>
-				<li className={s.menu__item}><a href='/profile'>Profile</a></li>
-				<li className={s.menu__item}><a href='/dialogs'>Messages</a></li>
-				<li className={s.menu__item}><a href='/news'>News</a></li>
-				<li className={s.menu__item}><a href='/music'>Music</a></li>
-				<li className={s.menu__item}><a href='/settings'>Settings</a></li>
+				<li className={s.menu__item}><NavLink to='/profile' className={navData => navData.isActive ? s.active : undefined}>Profile</NavLink></li>
+				<li className={s.menu__item}><NavLink to='/dialogs' className={navData => navData.isActive ? s.active : undefined}>Messages</NavLink></li>
+				<li className={s.menu__item}><NavLink to='/news' className={navData => navData.isActive ? s.active : undefined}>News</NavLink></li>
+				<li className={s.menu__item}><NavLink to='/music' className={navData => navData.isActive ? s.active : undefined}>Music</NavLink></li>
+				<li className={s.menu__item}><NavLink to='/settings' className={navData => navData.isActive ? s.active : undefined}>Settings</NavLink></li>
 			</ul>
-		</nav>
+		</nav >
 	)
 }
 export default Nav;
